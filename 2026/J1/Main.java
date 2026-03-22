fd499a330a5f08bfd43aeda115b1a039b26c876c
MrCliffordGRCI <154536770+MrCliffordGRCI@users.noreply.github.com>
2026-03-22 09:11:16 -0400

    Create Main.java

---
+++ 2026/J1/Main.java
@@ -0,0 +1,22 @@
+//2026 J1 
+
+import java.util.*;
+
+
+public class Main
+{
+	public static void main (String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		int B=scn.nextInt();
+		int T=scn.nextInt();
+		int P=scn.nextInt();
+		int difference=T-P; //this is the amount of tickets available to purchase
+		if(B>difference){
+		    System.out.println("N");
+		}else{
+		    System.out.println("Y "+(difference-B));
+		}
+
+	}
+}
